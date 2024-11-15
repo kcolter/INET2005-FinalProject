1c8c27839c5283ef02669d474fd11e35a213394c
Keegan <62590150+kcolter@users.noreply.github.com>
2024-11-15 15:40:06 -0400

    Scaffolded API

---
+++ app.js
@@ -0,0 +1,16 @@
+import express from 'express';
+
+const port = process.env.PORT || 3000;
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.send('Hello Express!');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
