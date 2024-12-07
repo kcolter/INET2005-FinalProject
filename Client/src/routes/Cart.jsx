f6e884a3c3c6418a6cd9de616274bc8ba451262d
Keegan <62590150+kcolter@users.noreply.github.com>
2024-12-06 23:25:45 -0400

    final commit for Sprint 3 submission, Cart page still needs work

--- Client/src/routes/Cart.jsx
+++ Client/src/routes/Cart.jsx
@@ -1,7 +1,88 @@
-export default function Cart(){
-    return (
-        <div>
-            <h1>Cart</h1>
-        </div>
-    );
+import { useParams, Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useCookies } from 'react-cookie';
+
+export default function Cart() {
+    const [cookies, setCookie] = useCookies(['BrickAndMortarCart']);
+    const [allProducts, setProducts] = useState([]);
+
+    useEffect(() => {
+        async function getAllProducts() {
+            const url = import.meta.env.VITE_APP_HOST + "/products/all";
+
+            //fetch data from api
+            const response = await fetch(url, { method: "GET" });
+
+            if (response.ok) {
+                const data = await response.json();
+                setProducts(data);
+            }
+        }
+
+        getAllProducts();
+    }, []);
+
+    var currentCookieVal = "" + cookies.BrickAndMortarCart; //get the cart's cookie
+
+    const cartArray = currentCookieVal.split(','); //split cookie into array
+
+    const itemCounts = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]; //works since we know only 10 products, in production would make length dynamic from count of products table 
+    var positionCount = 0; //to keep track of our position as we .map through
+
+    //similar process as in the products back-end, for each product increment it in the itemCounts
+    cartArray.map((item) => {
+        itemCounts[item]++;
+    });
+
+    var imgURL = "";
+
+    // Return early if products are not yet loaded, researched from GPT
+    if (allProducts.length === 0) {
+        return <div>Loading...</div>; // Show loading state
+    } else {
+
+        return (
+            <div>
+                <h1>Cart</h1>
+
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Product Name</th>
+                            <th>Unit Price</th>
+                            <th>Quantity</th>
+                            <th>Total</th>
+                            <th>Image</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+
+                        {itemCounts.map((item) => {
+                            if (item == 0) { //if count is 0, none in cart
+                                positionCount++;
+                                return; //return nothing, add no row to table
+                            } else {
+                                positionCount++;
+                                return <tr>
+                                    {imgURL = "/src/assets/images/" + allProducts[positionCount].image_filename}
+                                    <td>{allProducts[positionCount].name}</td>
+                                    <td>${allProducts[positionCount].cost}</td>
+                                    <td>{item}</td>
+                                    <td>{allProducts[positionCount].cost * itemCounts[positionCount]}</td>
+
+                                    <td><img src={imgURL}></img></td>
+                                </tr>
+                            }
+
+                        })}
+
+                    </tbody>
+                </table>
+
+
+                <Link to="/">Continue Shopping</Link>
+                <Link to="/checkout">Complete Purchase</Link>
+            </div>
+        );
+    }
 }
--- Client/src/routes/Details.jsx
+++ Client/src/routes/Details.jsx
@@ -1,20 +1,20 @@
 import { useParams, Link } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { useCookies } from 'react-cookie';
-export default function Details(){
+export default function Details() {
 
     const [cookies, setCookie] = useCookies(['BrickAndMortarCart']);
     const [productInfo, setProductInfo] = useState([]);
     const { id } = useParams();
 
-    useEffect(() =>{
-        async function getProductInfo(){
+    useEffect(() => {
+        async function getProductInfo() {
             const url = import.meta.env.VITE_APP_HOST + "/products/" + id;
 
             //fetch data from api
-            const response = await fetch(url, {method:"GET"});
+            const response = await fetch(url, { method: "GET" });
 
-            if(response.ok){
+            if (response.ok) {
                 const data = await response.json();
                 setProductInfo(data);
             }
@@ -24,15 +24,16 @@ export default function Details(){
     }, []);
 
     async function AddProductToCart() {
-        
+
         var currentCookieVal = "" + cookies.BrickAndMortarCart; //get existing cookie
         console.log(currentCookieVal);
 
-        if(currentCookieVal != "undefined"){
-        currentCookieVal += "," + productInfo.product_id; //append this products id
-        setCookie('BrickAndMortarCart', currentCookieVal, {path:'/', maxAge: 3600}) //re-set the cookie, timer is 1hr  
+        //if-logic to avoid having 'undefined' as first cart entry
+        if (currentCookieVal != "undefined") {
+            currentCookieVal += "," + productInfo.product_id; //append this products id
+            setCookie('BrickAndMortarCart', currentCookieVal, { path: '/', maxAge: 3600 }) //re-set the cookie, timer is 1hr  
         } else {
-            setCookie('BrickAndMortarCart', "" + productInfo.product_id, {path:'/', maxAge: 3600}); //if cookie is null, set it for the first time
+            setCookie('BrickAndMortarCart', "" + productInfo.product_id, { path: '/', maxAge: 3600 }); //if cookie is null, set it for the first time
         }
     }
 
